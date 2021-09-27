1df8daca5762336168ca23b752fa380b9dec4265
mortal <seregagl1996@gmail.com>
2021-09-27 16:09:12 +0300

    fitting page

--- #src/js/components/fitting.js
+++ #src/js/components/fitting.js
@@ -57,76 +57,86 @@ $('.fitting__sidebar-back').click(function (e) {
 if ($('._draggable').length > 0) {
     $('._draggable').draggable();
 
-    $('.fitting__main-control').click(function () {
-
-        if (!$(this).hasClass('_active')) {
-            $('.fitting__main-control').removeClass('_active');
-            $(this).addClass('_active');
-
-            if ($(this).hasClass('fitting__main-control--fluid-horizontal')) {
-                $('._draggable').draggable('enable');
-            } else {
-                $('._draggable').draggable('disable');
-            }
-        }
-    })
-
     $('._draggable').on('click', function () {
-        let transform = $(this).css('transform');
-
-
-        if ($('.fitting__main-control._active').hasClass('fitting__main-control--minimize')) {
-            $(this).width("-=10");
-            $(this).height("-=10");
-        } else if ($('.fitting__main-control._active').hasClass('fitting__main-control--maximize')) {
-            $(this).width("+=10");
-            $(this).height("+=10");
-        } else if ($('.fitting__main-control._active').hasClass('fitting__main-control--rotate-left')) {
-            var values = transform.split('(')[1],
-                values = values.split(')')[0],
-                values = values.split(',');
-
-            var a = values[0]; // 0.866025
-            var b = values[1]; // 0.5
-            var c = values[2]; // -0.5
-            var d = values[3]; // 0.866025
-
-            let angle = Math.round(Math.asin(b) * (180 / Math.PI));
-
-
-            if (angle >= -80) {
-                angle -= 10;
-                $(this).css({ 'transform': `rotate(${angle}deg)` });
-            }
-        }
-        else if ($('.fitting__main-control._active').hasClass('fitting__main-control--rotate-right')) {
-            var values = transform.split('(')[1],
-                values = values.split(')')[0],
-                values = values.split(',');
-
-            var a = values[0]; // 0.866025
-            var b = values[1]; // 0.5
-            var c = values[2]; // -0.5
-            var d = values[3]; // 0.866025
-
-            let angle = Math.round(Math.asin(b) * (180 / Math.PI));
-
-
-            if (angle <= 80) {
-                angle += 10;
-                $(this).css({ 'transform': `rotate(${angle}deg)` });
-            }
-        }
-        else if ($('.fitting__main-control._active').hasClass('fitting__main-control--close')) {
-            const data = $(this).attr('id');
-            $(`.small-basket__item[data-target="#${data}"]`).removeClass('_active');
-            $(this).removeClass('_active');
+        if (!$(this).hasClass('_hover')) {
+            $('._draggable').removeClass('_hover');
+            $(this).addClass('_hover');
+        } else {
+            $('._draggable').removeClass('_hover');
         }
     })
 
+    $('.fitting__main-control').on('click', function () {
+
+        if (!$('.fitting__main-item').hasClass('_hover')) {
+            alert('Пожалуйста, выберите Люстру');
+        } else {
+            if ($(this).hasClass('fitting__main-control--minimize')) {
+                $('.fitting__main-item._hover').width("-=10");
+                $('.fitting__main-item._hover').height("-=10");
+            } else if ($(this).hasClass('fitting__main-control--maximize')) {
+                $('.fitting__main-item._hover').width("+=10");
+                $('.fitting__main-item._hover').height("+=10");
+            } else if ($(this).hasClass('fitting__main-control--rotate-left')) {
+                let transform = $('.fitting__main-item._hover').css('transform');
+                let angle = getAngle(transform);
+
+                if (angle >= -80) {
+                    angle -= 10;
+                    if ($('.fitting__main-item._hover').hasClass('_flipped')) {
+                        $('.fitting__main-item._hover').css({ 'transform': `rotate(${angle}deg) scale(-1, 1)` });
+                    } else {
+                        $('.fitting__main-item._hover').css({ 'transform': `rotate(${angle}deg) scale(1, 1)` });
+                    }
+                }
+            } else if ($(this).hasClass('fitting__main-control--rotate-right')) {
+                let transform = $('.fitting__main-item._hover').css('transform');
+                let angle = getAngle(transform);
+
+                if (angle <= 80) {
+                    angle += 10;
+                    if ($('.fitting__main-item._hover').hasClass('_flipped')) {
+                        $('.fitting__main-item._hover').css({ 'transform': `rotate(${angle}deg) scale(-1, 1)` });
+                    } else {
+                        $('.fitting__main-item._hover').css({ 'transform': `rotate(${angle}deg) scale(1, 1)` });
+                    }
+                }
+            } else if ($(this).hasClass('fitting__main-control--close')) {
+                const data = $('.fitting__main-item._hover').attr('id');
+                $(`.small-basket__item[data-target="#${data}"]`).removeClass('_active');
+                $('.fitting__main-item._hover').removeClass('_active');
+            } else if ($(this).hasClass('fitting__main-control--fluid-horizontal')) {
+                let transform = $('.fitting__main-item._hover').css('transform');
+                let angle = getAngle(transform);
+                $('.fitting__main-item._hover').toggleClass('_flipped');
+
+                if ($('.fitting__main-item._hover').hasClass('_flipped')) {
+                    $('.fitting__main-item._hover').css({ 'transform': `rotate(${angle}deg) scale(-1, 1)` });
+                } else {
+                    $('.fitting__main-item._hover').css({ 'transform': `rotate(${angle}deg) scale(1, 1)` });
+                }
+            }
+        }
+
+    })
+
+    function getAngle(transform) {
+        let transformCss = transform;
+
+        var values = transformCss.split('(')[1],
+            values = values.split(')')[0],
+            values = values.split(',');
+
+        var a = values[0];
+        var b = values[1];
+        var c = values[2];
+        var d = values[3];
+
+        let angle = Math.round(Math.asin(b) * (180 / Math.PI));
+
+        return angle;
+    }
 }
 
-function getAngle() {
 
-}
 // === ONLINE-FITTING PAGE END ===
